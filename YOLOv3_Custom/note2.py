33d430e83823fb901c3fb2ff166d7e2805c4b789
playdata <zzpakg0312@gmail.com>
2021-04-22 14:51:24 +0900

    commit

--- YOLOv3_Custom/note2.py
+++ YOLOv3_Custom/note2.py
@@ -1,13 +1,6 @@
 import numpy as np
 
-import os
+a = []
 
-path = 'E:\\Computer Vision\\data\\project\\fruit_yolov3\\valid\\labels'
-id = os.listdir(path)
-
-for i in id:
-    with open(os.path.join(path, i), 'r') as f:
-        val = f.readline()
-        # print(val)
-        if val == '':
-            print(i)
+a.append(1, 2)
+print(a)
