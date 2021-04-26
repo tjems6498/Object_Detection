78b317a8b887d08e2d01798e89821cc86f28a584
playdata <zzpakg0312@gmail.com>
2021-04-27 00:16:27 +0900

    commit

--- YOLOv3_Custom/loss.py
+++ YOLOv3_Custom/loss.py
@@ -64,12 +64,10 @@ class YOLOLoss(nn.Module):
             (1e-16 + target[..., 3:5] / anchors)  # 분자가 0이 됨을 막기 위함
         )  # prediction의 tw,th를 bw,bh로 할 수 있지만 target을 오히려 역으로 바꿔 계산하면 better gradient flow를 갖는다고 한다.
 
-        box_loss = self.mse(predictions[..., 1:5][obj], target[..., 1:5][obj])  # same dim
+        #box_loss = self.mse(predictions[..., 1:5][obj], target[..., 1:5][obj])  # same dim
 
-        # giou_loss = generalized_intersection_over_union(predictions[..., 1:5][obj], target[..., 1:5][obj])  # GIoU loss
-        # box_loss = giou_loss
-
-
+        giou_loss = generalized_intersection_over_union(predictions[..., 1:5][obj], target[..., 1:5][obj])  # GIoU loss
+        box_loss = giou_loss
 
 
         # ================== #
