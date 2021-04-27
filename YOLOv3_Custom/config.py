e1df98e464011edb84432692763ea97d9d8274e8
playdata <zzpakg0312@gmail.com>
2021-04-27 11:18:32 +0900

    commit

--- YOLOv3_Custom/config.py
+++ YOLOv3_Custom/config.py
@@ -10,7 +10,7 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 seed_everything()  # deterministic behavior
 NUM_WORKERS = 2  # colab
-BATCH_SIZE = 3
+BATCH_SIZE = 2
 IMAGE_SIZE = 416
 NUM_CLASSES = 11
 CLASSES = ['apple', 'orange', 'pear', 'watermelon', 'durian', 'lemon', 'grapes', 'pineapple', 'dragon fruit', 'oriental melon', 'melon']
