908e473203163f14edc2146cae1abaed047df443
playdata <zzpakg0312@gmail.com>
2021-04-27 15:28:13 +0900

    commit

--- YOLOv3_Custom/note2.py
+++ YOLOv3_Custom/note2.py
@@ -6,6 +6,8 @@ import pdb
 from util import generalized_intersection_over_union
 import torch.nn as nn
 import math
+import torch
+import numpy as np
 
 # gt_bbox = torch.tensor([[0.25, 0.25, 0.5, 0.5]], dtype=torch.float32)
 # pr_bbox = torch.tensor([[0.75, 0.75, 0.5, 0.5]], dtype=torch.float32)
@@ -16,18 +18,18 @@ import math
 #
 # print(mse(pr_bbox, gt_bbox))
 
+ANCHORS = [
+    [(0.28, 0.22), (0.38, 0.48), (0.9, 0.78)],
+    [(0.07, 0.15), (0.15, 0.11), (0.14, 0.29)],
+    [(0.02, 0.03), (0.04, 0.07), (0.08, 0.06)],
+]
 
-def solution(brown, yellow):
-    x = int(((4+brown) + math.sqrt((4+brown)**2 - 16*(brown+yellow))) / 4)
-    y = int((brown + yellow) / x)
-
-    if x > y:
-        return [x, y]
-    else:
-        return [y, x]
-
-
-ans = solution(24, 24)
-print(ans)
+new_ANCHORS = [
+    [(130.83, 128.05), (180.67, 187.45), (309.66, 288.85)],
+    [(74.729, 73.366), (96.186, 92.182), (87.539, 138.52)],
+    [(24.245, 22.306), (39.584, 38.496), (56.897, 56.142)],
+]
 
 
+print(np.array(ANCHORS)*416)
+print(np.array(new_ANCHORS) / 416)
